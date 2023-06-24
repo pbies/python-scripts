b31052b49b7f6c49544e9304b060b2529e32e918
Piotr Biesiada <pmbiesiada@gmail.com>
2023-06-24 04:26:52 +0200

    fixes and improvements

---
+++ zip.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import zipfile
+from tqdm import tqdm
+
+wordlist = "..\\list.txt"
+zip_file = "dane.zip"
+
+zip_file = zipfile.ZipFile(zip_file)
+n_words = len(list(open(wordlist, "rb")))
+print("Total passwords to test:", n_words)
+
+with open(wordlist, "rb") as wordlist:
+	for word in tqdm(wordlist, total=n_words, unit="passwords"):
+		try:
+			zip_file.extractall(path='extracted',pwd=word.strip())
+		except:
+			continue
+		else:
+			print("[+] Password found:", word.decode().strip())
+			break
+
+print('Press Enter to quit...')
+input()
